3da1ee5eead6335c73b93e81f528d49ed80b76b5
kenkipkoec <itsmetrokenaki@gmal.com>
2024-08-09 11:55:03 +0300

    fixed bug

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -22,14 +22,14 @@ const Signup = () => {
 
   const navigate = useNavigate();
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     // Perform sign up logic here, then navigate
     if (data.accountType === "agent") {
       navigate("/agent");
     } else {
       navigate("/home");
     }
-    ry {
+    try {
       const response = await fetch('http://127.0.0.1:5000/signup', {
         method: 'POST',
         headers: {
