50bcacc0eca49dda58899a56dc094bffd2f210a5
kenkipkoec <itsmetrokenaki@gmal.com>
2024-08-09 12:02:02 +0300

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
-
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
   return (
     <Router>
-      <Navbar />
+      <Navbar isAuthenticated={isAuthenticated} user={user} />
       <Routes>
         <Route path="/" element={<HomePage />} /> 
+        <Route path="/rent" element={<Home />} />
+        <Route path="/buy" element={<Home />} />
+        <Route
+          path="/signup"
+          element={<Signup setIsAuthenticated={setIsAuthenticated} setUser={setUser} />}
+        />
+        <Route
+          path="/login"
+          element={<Login setIsAuthenticated={setIsAuthenticated} setUser={setUser} />}
+        />
+        <Route path="/agent" element={<AgentPage />} />
       </Routes>
     </Router>
   );
